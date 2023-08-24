09ad5f4b670b3b81786aedc4aa9b40d0dd093908
AvijitSen <senavijit1697@gmail.com>
2023-08-25 01:53:28 +0530

    send and recive msg using emit event and listen that event, create room and join this room using join()

--- Backend/app.js
+++ Backend/app.js
@@ -13,6 +13,24 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
     console.log(`A client connect with id: ${socket.id}`);
+
+    // emit joined event
+    socket.broadcast.emit("joined", { connectedId: socket.id });
+
+    // listent send msg
+    socket.on("send-message", (message, room) => {
+        if (room === "") {
+            socket.broadcast.emit("push-message", { pushedMessage: message });
+        } else {
+            socket.to(room).emit("push-message", { pushedMessage: message });
+        }
+    });
+
+    // join room event listen
+    socket.on("join-room", (room, cb) => {
+        socket.join(room);
+        cb();
+    });
 });
 
 const PORT = process.env.PORT || 3000;
--- Frontend/script.js
+++ Frontend/script.js
@@ -5,25 +5,41 @@ const form = document.getElementById("form");
 
 const socket = io("http://localhost:3000");
 
-socket.on("connect", (socket) => {
-    // const socketId = await socket.id;
-    displayMessage(`You are connect to server`);
+socket.on("connect", () => {
+    displayMessage(`You are connect to server with id: ${socket.id}`);
+});
+
+socket.on("joined", ({ connectedId }) => {
+    displayMessage(`${connectedId} joined`);
+});
+
+socket.on("push-message", ({ pushedMessage }) => {
+    console.log("pushedMessage: ", pushedMessage);
+    displayMessage(pushedMessage);
 });
 
 // Action
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const message = inputMsg.value;
+    if (message === "") return;
     inputMsg.value = "";
+
+    socket.emit("send-message", message, room.value);
     displayMessage(message);
 });
 
 createRoomBtn.addEventListener("click", () => {
-    console.log("create room clicked");
+    const roomName = room.value;
+    socket.emit("join-room", roomName, () => {
+        displayMessage(`you joined in room: ${roomName}`);
+    });
 });
 
 function displayMessage(msg) {
     const div = document.createElement("div");
+    div.className = "msg-div";
     div.textContent = msg;
     document.getElementById("message-container").append(div);
 }
